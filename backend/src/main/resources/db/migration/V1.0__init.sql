7648e39c1fb4ba32a2e90d7705b540583f9bb8e7
JanRuijtenberg <jan.ruijtenberg@hotmail.com>
2021-09-07 18:37:41 +0200

    Add flyway and let it create the database

---
+++ backend/src/main/resources/db/migration/V1.0__init.sql
@@ -0,0 +1,16 @@
+CREATE SEQUENCE public.hibernate_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+CREATE TABLE public.bookstore
+(
+    id bigint NOT NULL,
+    name character varying(256)
+);
+
+alter table public.bookstore add primary key (id);
+
