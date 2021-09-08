a868819269d0922bf9f2f2c1668480414c14511e
JanRuijtenberg <jan.ruijtenberg@hotmail.com>
2021-09-08 21:31:27 +0200

    finish the test on the bookstore api

--- backend/src/test/java/nl/jansolo/bookstore/api/BookControllerTest.java
+++ backend/src/test/java/nl/jansolo/bookstore/api/BookControllerTest.java
@@ -7,6 +7,9 @@ import static org.hamcrest.Matchers.equalTo;
 
 class BookControllerTest extends ApiTest {
 
+    final static long ISBN_JAVA_FOR_DUMMIES = 9788126568147l;
+    static final String JAVA_FOR_DUMMIES = "Java for dummies";
+
     private String getBaseUrl(){
         return getUrl("/book");
     }
@@ -24,7 +27,7 @@ class BookControllerTest extends ApiTest {
         .then()
                 .statusCode(200)
                 .body("$.size", equalTo(2))
-                .body("[0].title", equalTo("Java for dummies"))
+                .body("[0].title", equalTo(JAVA_FOR_DUMMIES))
                 .body("[1].title", equalTo("JavaScript: The Good Parts"));
     }
 
