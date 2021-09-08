14e3795694a1c2c8cc977ce53ab947c2ede4a096
JanRuijtenberg <jan.ruijtenberg@hotmail.com>
2021-09-08 18:31:08 +0200

    more strict security there is now a shopkeeper and a custommer role who have different authorities

--- backend/src/main/java/nl/jansolo/bookstore/security/DefaultWebSecurityConfiguration.java
+++ backend/src/main/java/nl/jansolo/bookstore/security/DefaultWebSecurityConfiguration.java
@@ -4,6 +4,7 @@ package nl.jansolo.bookstore.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,12 +15,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class DefaultWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private static final String ROLE_CUSTOMER = "customer";
+    private static final String ROLE_SHOPKEEPER = "shopkeeper";
 
-
-    @Value("${spring.security.user.name}")
-    private String username;
-    @Value("${spring.security.user.password}")
-    private String password;
+    @Value("${customer.user.name}")
+    private String customerUserName;
+    @Value("${customer.user.password}")
+    private String customerPassword;
+    @Value("${shopkeeper.user.name}")
+    private String shopkeeperUserName;
+    @Value("${shopkeeper.user.password}")
+    private String shopKeeperPassword;
 
     /**
      * Configures security for HTTP
@@ -36,19 +41,22 @@ public class DefaultWebSecurityConfiguration extends WebSecurityConfigurerAdapte
                 .authorizeRequests()
                 .antMatchers( "/h2-console/**").permitAll()
                 .antMatchers("/v3/api-docs/**", "/swagger*/**", "/webjars/**").permitAll()
-                .antMatchers("/bookstore/**").hasRole(ROLE_CUSTOMER)
-                .antMatchers("/book/**").hasRole(ROLE_CUSTOMER)
+                .antMatchers(HttpMethod.PUT, "/bookstore/*/order").hasAnyRole(ROLE_SHOPKEEPER)
+                .antMatchers(HttpMethod.PUT, "/bookstore/*/buy").hasAnyRole(ROLE_CUSTOMER)
+                .antMatchers(HttpMethod.GET, "/bookstore").hasAnyRole(ROLE_CUSTOMER, ROLE_SHOPKEEPER)
+                .antMatchers(HttpMethod.GET, "/book").hasAnyRole(ROLE_CUSTOMER, ROLE_SHOPKEEPER)
                 .anyRequest().denyAll()
                 .and()
                 .httpBasic();
     }
 
     /**
-     * loads a user into an in memory database with a password and username and the customer role
+     * loads a customer, and a shopkeeper user into an in memory database, should not be used in production
      */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser(username).password("{noop}"+password).roles(ROLE_CUSTOMER);
+        auth.inMemoryAuthentication().withUser(customerUserName).password("{noop}"+ customerPassword).roles(ROLE_CUSTOMER);
+        auth.inMemoryAuthentication().withUser(shopKeeperPassword).password("{noop}"+ shopKeeperPassword).roles(ROLE_SHOPKEEPER);
     }
 
 }
