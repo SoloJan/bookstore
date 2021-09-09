6240b9deefc68a942a32c4360a8d138ffde6a95e
JanRuijtenberg <jan.ruijtenberg@hotmail.com>
2021-09-09 12:05:28 +0200

    Add a developer role only the developer can acces the h2-console and the swagger ui

--- backend/src/main/java/nl/jansolo/bookstore/security/DefaultWebSecurityConfiguration.java
+++ backend/src/main/java/nl/jansolo/bookstore/security/DefaultWebSecurityConfiguration.java
@@ -16,6 +16,7 @@ public class DefaultWebSecurityConfiguration extends WebSecurityConfigurerAdapte
 
     private static final String ROLE_CUSTOMER = "customer";
     private static final String ROLE_SHOPKEEPER = "shopkeeper";
+    private static final String ROLE_DEVELOPER = "developer";
 
     @Value("${customer.user.name}")
     private String customerUserName;
@@ -25,13 +26,16 @@ public class DefaultWebSecurityConfiguration extends WebSecurityConfigurerAdapte
     private String shopkeeperUserName;
     @Value("${shopkeeper.user.password}")
     private String shopKeeperPassword;
-
+    @Value("${developer.user.name}")
+    private String developerUserName;
+    @Value("${developer.user.password}")
+    private String developerPassword;
     /**
      * Configures security for HTTP
      * Cors and csrf are disabled which should not be done in production
      * The customer and shopkeeper roles can both look at the available books and the stores
      * Only the customer can buy books, and only shopkeepers can order new books to his store
-     * The swagger documentation and H2 database are still accessible by everyone
+     * The swagger documentation and H2 database are only accessible by developers
      * @param http {@link HttpSecurity} instance to configure
      * @throws Exception exception
      */
@@ -42,8 +46,8 @@ public class DefaultWebSecurityConfiguration extends WebSecurityConfigurerAdapte
                 .csrf().disable().headers().frameOptions().disable()
                 .and()
                 .authorizeRequests()
-                .antMatchers( "/h2-console/**").permitAll()
-                .antMatchers("/v3/api-docs/**", "/swagger*/**", "/webjars/**").permitAll()
+                .antMatchers( "/h2-console/**").hasRole(ROLE_DEVELOPER)
+                .antMatchers("/v3/api-docs/**", "/swagger*/**", "/webjars/**").hasRole(ROLE_DEVELOPER)
                 .antMatchers(HttpMethod.PUT, "/bookstore/*/order").hasAnyRole(ROLE_SHOPKEEPER)
                 .antMatchers(HttpMethod.PUT, "/bookstore/*/buy").hasAnyRole(ROLE_CUSTOMER)
                 .antMatchers(HttpMethod.GET, "/bookstore").hasAnyRole(ROLE_CUSTOMER, ROLE_SHOPKEEPER)
@@ -54,12 +58,13 @@ public class DefaultWebSecurityConfiguration extends WebSecurityConfigurerAdapte
     }
 
     /**
-     * loads a customer, and a shopkeeper user into an in memory database, should not be used in production
+     * loads a customer,  a shopkeeper and a developer all with credentials into an in memory database, should not be used in production
      */
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser(customerUserName).password("{noop}"+ customerPassword).roles(ROLE_CUSTOMER);
         auth.inMemoryAuthentication().withUser(shopkeeperUserName).password("{noop}"+ shopKeeperPassword).roles(ROLE_SHOPKEEPER);
+        auth.inMemoryAuthentication().withUser(developerUserName).password("{noop}"+ developerPassword).roles(ROLE_DEVELOPER);
     }
 
 }
